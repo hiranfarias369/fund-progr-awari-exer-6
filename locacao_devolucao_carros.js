7a9c80afe71b6836694135b8f2616ec8250a94ac
hiranfarias369 <hira.farias.lima@gmail.com>
2022-12-25 13:28:07 -0300

    first commit

--- locacao_devolucao_carros.js
+++ locacao_devolucao_carros.js
@@ -112,8 +112,17 @@ class Esportivo extends Carro {
 		this.zero_a_cem = zero_a_cem;
 	}
 }
-const esportivo = new Esportivo("Camaro 6.2 Ss","2018","chevrolet", "esportivo conversível","2 portas","amarelo","32.987","R$ 489.74", "12s"
-)
+const esportivo = new Esportivo(
+	"Camaro 6.2 Ss",
+	"2018",
+	"chevrolet",
+	"esportivo conversível",
+	"2 portas",
+	"amarelo",
+	"32.987",
+	"R$ 489.74",
+	"12s"
+);
 class Pessoa {
 	constructor(nome, idade, data_nascimento, endereco, telefone_celular, email) {
 		this.nome = nome;
@@ -124,15 +133,59 @@ class Pessoa {
 		this.email = email;
 	}
 }
+class Cliente extends Pessoa {
+	constructor(
+		nome,
+		idade,
+		data_nascimento,
+		endereco,
+		telefone_celular,
+		email,
+		cpf,
+		numero_carteira_motorista,
+		foto_carteira_motorista,
+		ano_vencimento_carteira_motorista
+	) {
+		super(
+			nome,
+			idade,
+			data_nascimento,
+			endereco,
+			telefone_celular,
+			email,
+			cpf,
+			numero_carteira_motorista,
+			foto_carteira_motorista,
+			ano_vencimento_carteira_motorista
+		);
+		this.cpf = cpf;
+		this.numero_carteira_motorista = numero_carteira_motorista;
+		this.foto_carteira_motorista = foto_carteira_motorista;
+		this.ano_vencimento_carteira_motorista = ano_vencimento_carteira_motorista;
+	}
+}
+const cliente = new Cliente(
+	"Antonio Treze de Junho de Mil Novecentos e Dezessete",
+	"94 anos",
+	"12/12/1922",
+	"Santa Rita do Passa Quatro, São Paulo",
+	"(011) 9 9999-999",
+	"antoniotreze@gmail.com",
+	"82112150120",
+	"2354242531",
+	"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fbr%2Ffoto%2Fmonumento-ao-folclore-sergipano-esculturas-de-personagens-folcl%25C3%25B3ricos-localizados-no-gm1190708941-337644314&psig=AOvVaw25kW1ty5Yn_fUbBjXACcg5&ust=1672071844069000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJiAtZOXlfwCFQAAAAAdAAAAABAE",
+	"2032"
+);
 const pessoa1 = new Pessoa(
 	"Yanka",
-	"94 anos",
+	"100 anos",
 	"12/12/2012",
 	"Santa Rita do Passa Quatro, São Paulo",
 	"(011) 9 9999-999",
 	"yanka@gmail.com"
 );
+//console.log(pessoa1);
 
 console.log(utilitario);
 console.log(esportivo);
-console.log(pessoa1);
+console.log(cliente);
